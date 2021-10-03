a483291fd95ce91556bb23d493dc6c0d37d382a7
Jim Cramer <remarcmij@gmail.com>
2021-10-03 12:55:06 +0200

    Check for rejected settings updates

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,39 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 
+async function applySettings() {
+  try {
+    const json = await fs.promises.readFile(
+      path.join(__dirname, '../data/user-settings.json'),
+      'utf8'
+    );
+    const targetSettings = JSON.parse(json);
+    const config = vscode.workspace.getConfiguration();
+    const promises = Object.entries(targetSettings).map(([section, value]) => {
+      return config.update(section, value, vscode.ConfigurationTarget.Global);
+    });
+    const results = await Promise.allSettled(promises);
+    results.forEach((result) => {
+      if (result.status === 'rejected') {
+        vscode.window.showErrorMessage(result.reason.message);
+      }
+    });
+    if (results.some((result) => result.status === 'rejected')) {
+      vscode.window.showWarningMessage(
+        'Some recommended VSCode settings could not be applied successfully.'
+      );
+    } else {
+      vscode.window.showInformationMessage(
+        'All recommended VSCode settings have been applied successfully.'
+      );
+    }
+  } catch {
+    vscode.window.showErrorMessage(
+      'An error occurred while attempting to apply the recommended VSCode settings.'
+    );
+  }
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -16,29 +49,9 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
+  const disposable = vscode.commands.registerCommand(
     'hyf-vscode-config.applySettings',
-    async () => {
-      try {
-        const json = await fs.promises.readFile(
-          path.join(__dirname, '../data/user-settings.json'),
-          'utf8'
-        );
-        const targetSettings = JSON.parse(json);
-        const config = vscode.workspace.getConfiguration();
-        const promises = Object.entries(targetSettings).map(([key, value]) => {
-          return config.update(key, value, vscode.ConfigurationTarget.Global);
-        });
-        await Promise.all(promises);
-        vscode.window.showInformationMessage(
-          'HackYourFuture recommended VSCode settings applied successfully.'
-        );
-      } catch {
-        vscode.window.showErrorMessage(
-          'An error occurred while attempting to apply the HackYourFuture recommended VSCode settings.'
-        );
-      }
-    }
+    applySettings
   );
 
   context.subscriptions.push(disposable);
