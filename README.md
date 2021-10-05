055b22c82f522c563c0c61c8d60a8421cf3a5ffb
Jim Cramer <remarcmij@gmail.com>
2021-10-05 15:11:03 +0200

    First published version

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # HackYourFuture VSCode Config Helper
 
-This VSCode extension is intended for use by students enrolled in the [HackYourFuture](https://www.hackyourfuture.net/) curriculum. It helps to apply the recommended VSCode user settings for the curriculum by means of a simple VSCode command.
+This VSCode extension is intended for use by students enrolled in the [HackYourFuture](https://www.hackyourfuture.net/) curriculum.
 
-To apply the settings, open the VSCode Command Palette (Ctrl+Shift+P), and select the command:
+- It installs a recommended basic set of VSCode extensions.
+- It adds a command to apply recommended user settings.
+- It adds the **Ctrl+Shift+I** keyboard shortcut to toggle [parameter parameter inlay hints](https://code.visualstudio.com/updates/v1_60#_inlay-hints-for-javascript-and-typescript) on and off.
+
+The extensions bundled with this VSCode extension are:
+
+- [Better Comments](https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments)
+- [Code Spell Checker](https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.code-spell-checker)
+- [ESLint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint)
+- [Live Server](https://marketplace.visualstudio.com/items?itemName=hoovercj.vscode-settings-cycler)
+- [Prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode)
+- [Settings Cycler\*](https://marketplace.visualstudio.com/items?itemName=hoovercj.vscode-settings-cycler)
+
+To apply the recommended settings, open the VSCode Command Palette (**Ctrl+Shift+P)**, and select the command:
 
 ```text
 HackYourFuture: Apply recommended VSCode Settings
--- src/extension.ts
+++ src/extension.ts
@@ -4,52 +4,62 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 
+async function applyRecommendedSettings() {
+  const json = await fs.promises.readFile(
+    path.join(__dirname, `../data/user-settings.json`),
+    'utf8'
+  );
+  const targetSettings = JSON.parse(json);
+
+  const config = vscode.workspace.getConfiguration();
+
+  const promises = Object.entries(targetSettings).map(([section, value]) => {
+    return config.update(section, value, vscode.ConfigurationTarget.Global);
+  });
+
+  const results = await Promise.allSettled(promises);
+
+  let allOkay = true;
+  results.forEach((result) => {
+    if (result.status === 'rejected') {
+      vscode.window.showErrorMessage(result.reason.message);
+      allOkay = false;
+    }
+  });
+
+  if (allOkay) {
+    vscode.window.showInformationMessage(
+      'The HackYourFuture recommended VSCode settings have been applied successfully.'
+    );
+  } else {
+    vscode.window.showWarningMessage(
+      'Some HackYourFuture recommended VSCode settings could not be applied successfully.'
+    );
+  }
+}
+
+async function removeExplicitJavaScriptFormatterSetting() {
+  const config = vscode.workspace.getConfiguration();
+
+  // Check for a JavaScript language specific formatter setting
+  const javaScriptLanguageSection =
+    config.get<Record<string, unknown>>('[javascript]');
+
+  if (javaScriptLanguageSection?.['editor.defaultFormatter']) {
+    // Remove JavaScript specific formatter setting and revert to the global
+    // default of Prettier
+    await config.update(
+      'javascript.editor.defaultFormatter',
+      undefined,
+      vscode.ConfigurationTarget.Global
+    );
+  }
+}
+
 async function applySettings() {
   try {
-    const json = await fs.promises.readFile(
-      path.join(__dirname, '../data/user-settings.json'),
-      'utf8'
-    );
-    const targetSettings = JSON.parse(json);
-
-    const config = vscode.workspace.getConfiguration();
-
-    // Check for a JavaScript language specific formatter setting
-    const javaScriptLanguageSection = config.get('[javascript]') as
-      | Record<string, unknown>
-      | undefined;
-
-    if (javaScriptLanguageSection?.['editor.defaultFormatter']) {
-      // Remove JavaScript specific formatter setting and revert to the global
-      // default of Prettier
-      targetSettings['[javascript]'] = {
-        'editor.defaultFormatter': undefined,
-      };
-    }
-
-    const promises = Object.entries(targetSettings).map(([section, value]) => {
-      return config.update(section, value, vscode.ConfigurationTarget.Global);
-    });
-
-    const results = await Promise.allSettled(promises);
-
-    let allOkay = true;
-    results.forEach((result) => {
-      if (result.status === 'rejected') {
-        vscode.window.showErrorMessage(result.reason.message);
-        allOkay = false;
-      }
-    });
-
-    if (allOkay) {
-      vscode.window.showInformationMessage(
-        'All recommended VSCode settings have been applied successfully.'
-      );
-    } else {
-      vscode.window.showWarningMessage(
-        'Some recommended VSCode settings could not be applied successfully.'
-      );
-    }
+    await applyRecommendedSettings();
+    await removeExplicitJavaScriptFormatterSetting();
   } catch (error: any) {
     vscode.window.showErrorMessage(
       `An error occurred while attempting to apply the recommended VSCode settings: ${error.message}`
@@ -62,9 +72,7 @@ async function applySettings() {
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "hyf-vscode-config" is now active!'
-  );
+  console.log('Extension "hyf-vscode-config" is now active.');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
